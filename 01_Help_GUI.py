d88abd24288a8e10b5d3567230c28fbede705fe3
giddycaleb <66811778+giddycaleb@users.noreply.github.com>
2022-03-17 09:53:04 +1300

    Updated Hel GUI

---
+++ 01_Help_GUI.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+class Convertor:
+    def __init__(self,parent):
+        print("Hello World")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Convertor")
+    something = Convertor(root)
+    root.mainloop()
+
